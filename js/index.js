68778359366bce7cd1d56cea30e7c15e5e50ba20
666 <kamagaspar@gmail.com>
2021-05-12 19:25:26 -0400

    tynimc

--- js/index.js
+++ js/index.js
@@ -1,4 +1,22 @@
 
+tinymce.init({
+    selector: '#descripcion-txt',
+    height: 500,
+    menubar: false,
+    plugins: [
+      'advlist autolink lists link image charmap print preview anchor',
+      'searchreplace visualblocks code fullscreen',
+      'insertdatetime media table paste code help wordcount'
+    ],
+    toolbar: 'undo redo | formatselect | ' +
+    'bold italic backcolor | alignleft aligncenter ' +
+    'alignright alignjustify | bullist numlist outdent indent | ' +
+    'removeformat | help',
+    content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+  });
+  
+  
+
 document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     let nombre = document.querySelector("#nombre-txt").value;
     console.log("Hola Mundo!", nombre);
