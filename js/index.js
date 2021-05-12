1514f96eb43c57a9fb1c59c2682be09812d9d1b5
666 <kamagaspar@gmail.com>
2021-05-12 02:31:38 -0400

    comienzo js

---
+++ js/index.js
@@ -0,0 +1,5 @@
+
+document.querySelector("#registrar-btn").addEventListener("click", ()=>{
+    let nombre = document.querySelector("#nombre-txt").value;
+    console.log("Hola Mundo!", nombre);
+} );
